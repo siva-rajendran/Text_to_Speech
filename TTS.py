53cce0e54289148cad1850479ba7d5d6ee5b7e63
gokul1630 <gokulgp163034@gmail.com>
2019-12-31 18:22:01 +0530

    initial commit

---
+++ TTS.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+
+import os
+
+a ='Ta'
+
+f = open("kural.txt", encoding="utf8")
+
+m = f.read()
+
+c=gTTS(lang=a, text=m, slow=False)
+
+c.save("Thirukkural.mp3")
+
+os.system("Thirukkural.mp3")
